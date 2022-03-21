c7891b91a0ef49f0b31c811789abaf2fa2ccf241
fernandoprestes <fernandoprestesc@gmail.com>
2022-03-21 20:56:06 -0300

    initial config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,vue}', './src/**/*', './index.html'],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
