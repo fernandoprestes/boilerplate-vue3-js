aca4c51ed8f601dbf80165fb073569b9f64a867a
fernandoprestes <fernandoprestesc@gmail.com>
2022-07-08 18:32:54 -0300

    feat: add content about the project

---
+++ src/components/Icon/ViteVueColors.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>
+    <a
+      href="https://vitejs.dev/"
+      target="_blank"
+      class="vite-color font-bold"
+    >
+      Vite
+    </a>
+    +
+    <a
+      href="https://vuejs.org/"
+      target="_blank"
+      class="font-bold text-[#3fb17f]"
+    >
+      VueJS
+    </a>
+  </span>
+</template>
+<style lang="css" scoped>
+  .vite-color {
+    background: -webkit-linear-gradient(top left, #5eacff, #936afe, #ae47fe);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+</style>
--- src/pages/About/index.vue
+++ src/pages/About/index.vue
@@ -1,40 +1,96 @@
+<script setup>
+  import { ref } from 'vue';
+  import ViteVueColors from './../../components/Icon/ViteVueColors.vue';
+
+  const count = ref(0);
+
+  function increment() {
+    count.value += 1;
+  }
+
+  function decrement() {
+    if (count.value === 0) return;
+    count.value -= 1;
+  }
+</script>
 <template>
-  <div class="h-screen">
-    <div class="container pt-10">
-      <button
-        class="flex items-center justify-center gap-2 hover:text-primary-300"
-        @click="$router.push({ name: 'Home' })"
-      >
-        <span
-          class="iconify"
-          data-icon="ic:baseline-arrow-back"
-        ></span
-        >Voltar
-      </button>
+  <div class="h-full min-h-screen py-10">
+    <div class="container max-w-2xl px-8">
+      <div class="my-6">
+        <button
+          class="flex items-center justify-center gap-2 font-bold text-slate-400 hover:text-white"
+          @click="$router.push({ name: 'Home' })"
+        >
+          <span
+            class="iconify"
+            data-icon="ic:baseline-arrow-back"
+          />
+          Voltar
+        </button>
+      </div>
+      <div class="space-y-4 text-xl leading-relaxed text-slate-300">
+        <p>
+          Este projeto de boilerplate <ViteVueColors /> foi desenvolvido com base em estudos sobre o Vue e tem como
+          objetivo servir de template para projetos de estudo que me faça especializar no desenvolvimento frontend.
+        </p>
+        <p>
+          Sempre que precisava iniciar um <span class="rounded bg-slate-600 px-2 font-mono">setup</span> para um projeto
+          para poder praticar ou estudar tinha que fazer as mesmas configurações todas as vezes. Pensando nisso, resolvi
+          criar este template para ser sempre o ponto inicial de todos os projetos que eu quisesse ou precisasse fazer.
+        </p>
+        <p>
+          Como o avanço e aprofundamento nos estudos, e para padronizar todos os meus projetos, estudei e adicionei o
+          <a
+            href="https://eslint.org/"
+            target="_blank"
+            class="inline-block rounded bg-slate-300 px-2 font-mono font-bold text-[#482fbd]"
+          >
+            ESLint
+          </a>
+          e o
+          <a
+            href="https://prettier.io/"
+            target="_blank"
+            class="inline-block rounded bg-slate-300 px-2 font-mono font-bold text-[#1a2b34]"
+          >
+            Prettier
+          </a>
+          para ter um padrão de código ao meu estilo. Também estudei e adicionei o
+          <a
+            href="https://tailwindcss.com/"
+            target="_blank"
+            class="font-bold text-[#38bdf8]"
+          >
+            tailwindcss
+          </a>
+          , framework css para desenvolvimento rápido.
+        </p>
+
+        <p>
+          Um caso de uso desse boilerplate foi o desenvolvimento do projeto
+          <a
+            href="https://github.com/fernandoprestes/cake-shop-client"
+            class="underline underline-offset-2 hover:text-black"
+          >
+            cake-shop-client
+          </a>
+          o projeto foi iniciado usando este boilerplate.
+        </p>
+      </div>
     </div>
-    <div class="container w-1/2 space-y-4">
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit quaerat nam laudantium cum ea cupiditate!
-        Nobis, perspiciatis molestiae molestias eius iure quaerat libero numquam voluptatibus, laborum suscipit quasi
-        atque perferendis?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quae corrupti, provident iste et quidem sequi? Dolor
-        aperiam sint magnam?
-      </p>
-    </div>
-    <div class="container mt-10 flex flex-col items-center justify-center space-y-10">
-      <span class="text-4xl">{{ count }}</span>
+    <div class="container mt-10 flex flex-col items-center justify-center space-y-4 px-8">
+      <p class="font-mono text-slate-300">Exemplo para incrementar e decrementar um contador</p>
+      <span class="text-4xl text-white">{{ count }}</span>
       <span>{{ countLimit }}</span>
       <div class="flex gap-2">
         <button
-          class="rounded bg-primary-600 py-2 px-6 text-white"
+          class="rounded bg-primary-600 py-2 px-6 text-white hover:bg-primary-500"
           @click="increment"
         >
           Incrementar
         </button>
         <button
-          class="rounded bg-primary-600 py-2 px-6 text-white"
+          class="rounded bg-primary-600 py-2 px-6 text-white hover:bg-primary-500"
           @click="decrement"
         >
           Decrementar
@@ -43,21 +99,3 @@
     </div>
   </div>
 </template>
-<script>
-  export default {
-    data() {
-      return {
-        count: 0,
-      };
-    },
-    methods: {
-      increment() {
-        this.count += 1;
-      },
-      decrement() {
-        if (this.count === 0) return;
-        this.count -= 1;
-      },
-    },
-  };
-</script>
