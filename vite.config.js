210cdcc2acd967ba6cb065cdf73132e78fd554ec
fernandoprestes <fernandoprestesc@gmail.com>
2022-09-23 21:31:09 -0300

    update deps

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'url';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: 4000,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
